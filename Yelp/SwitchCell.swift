4b830b3a13040612e0f7ac13ef0944c7309b47af
“pjaswa1” <pjaswal@walmartlabs.com>
2016-07-24 17:45:36 -0700

    Adding filterViewController

---
+++ Yelp/SwitchCell.swift
@@ -0,0 +1,39 @@
+//
+//  SwitchCell.swift
+//  Yelp
+//
+//  Created by Pushpinder Jaswal on 7/24/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol SwitchCellDelegate {
+    optional func switchCell(switchCell: SwitchCell, didChangeValue value : Bool)
+}
+
+class SwitchCell: UITableViewCell {
+
+    @IBOutlet weak var switchLabel: UILabel!
+    @IBOutlet weak var onSwitch: UISwitch!
+    
+    weak var delegate: SwitchCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        onSwitch.addTarget(self, action: "switchValueChanged", forControlEvents: UIControlEvents.ValueChanged)
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func switchValueChanged(){
+        delegate?.switchCell?(self, didChangeValue: onSwitch.on)
+    }
+
+}
